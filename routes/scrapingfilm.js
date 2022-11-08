2f4993e042f09f195d9bda419488edc88e817349
khanhchi <le.khanhchi@yahoo.fr>
2022-11-08 16:44:36 +0100

    get movies from server, store in vuex, show info in ComingSoon

--- routes/scrapingfilm.js
+++ routes/scrapingfilm.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const filmCtrl = require('../controllers/scrapingfilm')
 
-router.get('/:dateChoosen', filmCtrl.scrapingfilm)
+router.get('/', filmCtrl.scrapingfilm)
+router.post('/:dateChoosen', filmCtrl.filmsSelect)
 
 module.exports = router;
